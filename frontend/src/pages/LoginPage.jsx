2e583d04f39a5a5454555edf454233837dbc1e7b
XxpolipxX <132904175+XxpolipxX@users.noreply.github.com>
2025-11-20 14:57:06 +0100

    Update LoginPage.jsx

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
 import AuthForm from "../components/AuthForm";
+import handleLogin from "../services/handleLogin";
 
 export default function LoginPage() {
   return (
@@ -6,13 +7,14 @@ export default function LoginPage() {
       title="TODO List"
       subtitle="Zaloguj się, aby kontynuować"
       fields={[
-        { type: "text", placeholder: "Nazwa użytkownika", required: true },
-        { type: "password", placeholder: "Hasło", required: true, minLength: 8 },
+        { type: "text", placeholder: "Nazwa użytkownika", name: 'login', required: true },
+        { type: "password", placeholder: "Hasło", name: 'password', required: true, minLength: 8 },
       ]}
       submitLabel="Zaloguj się"
       bottomText="Nie masz jeszcze konta?"
       bottomLink="/register"
       bottomLinkLabel="Zarejestruj się"
+      onSubmit={({ login, password }) => handleLogin(login, password)}
     />
   );
 }
